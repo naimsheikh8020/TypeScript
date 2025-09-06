87e49ef4e44aa65219588407fdf98a9165fda761
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-09-07 01:40:04 +0600

    Object optional and literal types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,14 @@
+//Reference Typescript 
+
+const user :{
+    readonly company: 'Programming Hero' //type --> literal types
+    firstName: string;
+    middleName?: string;//optional Type
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'Programming Hero',
+    firstName: "Naim ",
+    lastName: "Sheikh",
+    isMarried: true,
+}
